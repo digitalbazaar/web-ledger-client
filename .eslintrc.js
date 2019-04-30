ae8f9b32ab29da8afc7fd384c74171abd0272dfa
Matthew Collier <mcollier@digitalbazaar.com>
2019-04-30 10:44:56 -0400

    Use eslint-config-digitalbazaar@2.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,6 @@
 module.exports = {
   root: true,
-  extends: [
-    'eslint-config-digitalbazaar',
-    'eslint-config-digitalbazaar/jsdoc'
-  ],
+  extends: ['digitalbazaar', 'digitalbazaar/jsdoc'],
   env: {
     node: true
   }
